5ce7587237e36a44de0a5debb4bedec3c4228370
drmats <drmats@users.noreply.github.com>
2018-05-07 13:09:49 +0200

    utils/choose() added.

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+// functional replacement of "switch" statement
+export const choose = (
+    key,
+    actions = {},
+    defaultAction = () => null,
+    args = []
+) =>
+    key in actions ?
+        actions[key](...args) :
+        defaultAction(...args)
