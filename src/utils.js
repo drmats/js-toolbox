470426a1a04a3736e162949d4e3a7d33e9ac942c
drmats <drmats@users.noreply.github.com>
2018-05-07 13:13:45 +0200

    utils/handleException() added.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
+// helper - handle exceptions in expressions
+export const handleException = (fn, handler) => {
+    try { return fn() }
+    catch (ex) { return typeof handler === "function" ? handler(ex) : ex }
+}
+
+
+
+
 // functional replacement of "switch" statement
 export const choose = (
     key,
