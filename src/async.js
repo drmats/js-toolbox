1a8c75c923c3da06af2366171fac1c913d519813
drmats <drmats@users.noreply.github.com>
2018-05-08 14:33:56 +0200

    async.js extracted from utils.js.

---
+++ src/async.js
@@ -0,0 +1,33 @@
+// setTimeout in promise/async skin
+// example usage:
+//
+// sf.utils.timeout(
+//     () => { console.log("Hey!"); return 42 }, 1000,
+//     (c) => sf.utils.timeout(() => c("Cancelled!"), 800)
+// )
+// .then((x) => console.log("Success:", x))
+// .catch((c) => console.log("Error or cancel:", c))
+//
+export const timeout = (f, time = 1000, cancel = (_reason) => null) => {
+    let
+        handle = null, reject = null,
+        promise = new Promise((res, rej) => {
+            reject = rej
+            handle = setTimeout(() => {
+                try { res(f()) }
+                catch (ex) { rej(ex) }
+            }, time)
+        })
+    cancel((reason) => {
+        clearTimeout(handle)
+        reject(reason)
+    })
+    return promise
+}
+
+
+
+
+// convenience shortcut of "timeout"
+export const delay = (time = 1000, cancel = (_reason) => null) =>
+    timeout(() => time, time, cancel)
