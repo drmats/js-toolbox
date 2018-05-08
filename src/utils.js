f5de497a671445191d837ff3743e91eea86ca07b
drmats <drmats@users.noreply.github.com>
2018-05-08 15:31:40 +0200

    utils: range() added.

--- src/utils.js
+++ src/utils.js
@@ -27,7 +27,8 @@ export const createReducer = (initState = {}) =>
 
 
 // construct object from result of Object.entries() call
-// entries = [[k1,v1], ... [kn, vn]]
+// entries = [[k1, v1], ... [kn, vn]]
+//
 // imitates Python's dict()
 export const dict = (entries) => entries.reduce(
     (acc, [k, v,]) => ({ ...acc, [k]: v, }), {}
@@ -59,3 +60,40 @@ export const swap = (o) => dict(
         .entries(o)
         .map((kv) => [].concat(kv).reverse())
 )
+
+
+
+
+// range(stop) -> array of integers
+// range(start, stop[, step]) -> array of integers
+//
+// Return a list containing an arithmetic progression of integers.
+// range(i, j) returns [i, i+1, i+2, ..., j-1]; start defaults to 0.
+// When step is given, it specifies the increment (or decrement).
+// For example, range(4) returns [0, 1, 2, 3].
+//
+// imitates Python's range()
+export const range = (...args) => {
+    let start = 0, stop = 0, step = 1, arr = []
+
+    if (args.length === 1) { [stop,] = args }
+    else if (args.length === 2) { [start, stop,] = args }
+    else if (args.length === 3) {
+        [start, stop, step,] = args
+        if (step === 0) throw new RangeError(
+            "range() 'step' argument must not be zero"
+        )
+    } else throw new TypeError(
+        `range() expected at most 3 arguments, got ${args.length}`
+    )
+
+    while (
+        (start < stop  &&  step > 0)  ||
+        (start > stop  &&  step < 0)
+    ) {
+        arr[arr.length] = start
+        start += step
+    }
+
+    return arr
+}
