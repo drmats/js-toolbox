f51e952d7cab2359418a5ebddb9b7a987661adc0
drmats <drmats@users.noreply.github.com>
2018-05-10 12:09:34 +0200

    array: sparse() added.

--- src/array.js
+++ src/array.js
@@ -1,5 +1,5 @@
 //
-// return first element of the given array
+// Return first element of the given array.
 //
 export const first = (arr) => arr[0]
 
@@ -7,7 +7,7 @@ export const first = (arr) => arr[0]
 
 
 //
-// simple array flattener
+// Simple array flattener.
 // [[1, 2,], ..., [3, 4,],]  ->  [1, 2, ..., 3, 4,]
 //
 export const flatten = (arr) => arr.reduce((acc, el) => acc.concat(el), [])
@@ -16,7 +16,7 @@ export const flatten = (arr) => arr.reduce((acc, el) => acc.concat(el), [])
 
 
 //
-// return last element of the given array
+// Return last element of the given array.
 //
 export const last = (arr) => arr[arr.length-1]
 
@@ -68,18 +68,55 @@ export const range = (...args) => {
 // The operation is taken in-place.
 //
 export const shuffle = (arr) => {
-    let j, tmp
-
     if (!Array.isArray(arr)) throw new TypeError(
         `shuffle() expected array as argument, got ${typeof arr}`
     )
 
     for (let i = arr.length-1;  i > 0;  i -= 1) {
-        j = Math.floor(Math.random()*1e16) % (i+1)
-        tmp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = tmp
+        let j = Math.floor(Math.random()*1e16) % (i+1);
+        [arr[i], arr[j],] = [arr[j], arr[i],]
     }
 
     return arr
+}
+
+
+
+
+//
+// sparse(stop, size) -> array of 'size' distinct integers
+//     in range [0..stop]
+// sparse(start, stop, size) -> array of 'size' distinct integers
+//     in range [start..stop]
+//
+// Generate sparse array of distinct integers
+// with (almost) uniform distribution.
+//
+export const sparse = (...args) => {
+    let
+        start = 0, stop = 0, size = 0,
+        hash = Object.create(null),
+        interval = 0
+
+    if (args.length === 2) { [stop, size,] = args }
+    else if (args.length === 3) { [start, stop, size,] = args }
+    else throw new TypeError(
+        `sparse() expected 2 or 3 arguments, got ${args.length}`
+    )
+
+    if (start > stop) { [start, stop,] = [stop, start,] }
+    interval = stop - start
+
+    if (size <= 0  ||  interval === 0) { return [] }
+    if (size >= interval) { return range(start, stop) }
+
+    while (size > 0) {
+        let val = (Math.floor(Math.random()*1e16) % interval) + start
+        if (!Object.hasOwnProperty.call(hash, val)) {
+            hash[val] = val
+            size -= 1
+        }
+    }
+
+    return Object.values(hash).sort((a, b) => a - b)
 }
