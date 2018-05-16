12fe21af5ec1b7873244107685395758830f42e7
drmats <drmats@users.noreply.github.com>
2018-05-16 09:16:06 +0200

    src/index.js added.

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// ...
+export {
+    flatten,
+    head,
+    init,
+    last,
+    range,
+    shuffle,
+    sparse,
+    tail,
+} from "./array"
+
+// ...
+export {
+    delay,
+    interval,
+    timeout,
+} from "./async"
+
+// ...
+export {
+    log10,
+    log2,
+    roundIfClose,
+} from "./math"
+
+// ...
+export {
+    createReducer,
+} from "./redux"
+
+// ...
+export {
+    big as bigString,
+    camelToPascal,
+    camelToSnake,
+    capitalize,
+    empty as emptyString,
+    pascalToCamel,
+    pascalToSnake,
+    snakeToCamel,
+    snakeToPascal,
+} from "./string"
+
+// ...
+export {
+    choose,
+    dict,
+    handleException,
+    swap,
+    Y,
+} from "./utils"
