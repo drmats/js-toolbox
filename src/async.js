2de10d58bd2ae6b311b5838426dac35baa8bbded
drmats <drmats@users.noreply.github.com>
2018-05-29 20:03:13 +0200

    async: jsdoc-style comments added.

--- src/async.js
+++ src/async.js
@@ -1,23 +1,59 @@
-//
-// Convenience shortcut of "timeout".
-//
-export const delay = (time = 1000, cancel = (_reason) => null) =>
+/**
+ * Asynchronous tools.
+ *
+ * @module async
+ * @license Apache-2.0
+ */
+
+
+
+
+/**
+ * Delay current async execution by `time` miliseconds.
+ *
+ * ```
+ * (async () => {
+ *     await async.delay()
+ *     console.log("Hello ...")
+ *     await async.delay()
+ *     console.log("... world")
+ * })()
+ * ```
+ *
+ * @async
+ * @function delay
+ * @see {@link async.timeout}
+ * @param {Number} [time=1000]
+ * @param {Function} [cancel]
+ * @returns {Promise}
+ */
+export const delay = (time = 1000, cancel = (_canceller) => null) =>
     timeout(() => time, time, cancel)
 
 
 
 
-//
-// setInterval in promise/async skin.
-// Example usage:
-//
-// interval(
-//     () => { console.log("Hey!"); return 42 },
-//     (c) => timeout(() => c(), 4000)
-// )
-// .then((x) => console.log("Finished:", x))
-// .catch((c) => console.log("Error:", c))
-//
+/**
+ * `setInterval` in `Promise` / `async` skin.
+ *
+ * Example usage:
+ *
+ * ```
+ * interval(
+ *     () => { console.log("Hey!"); return 42 },
+ *     (c) => timeout(() => c(), 4000)
+ * )
+ * .then((x) => console.log("Finished:", x))
+ * .catch((c) => console.log("Error:", c))
+ * ```
+ *
+ * @async
+ * @function interval
+ * @param {Function} f
+ * @param {Function} clear
+ * @param {Number} [time=1000]
+ * @returns {Promise}
+ */
 export const interval = (f, clear, time = 1000) => {
     let
         resolve = null, handle = null, result = null,
@@ -39,17 +75,27 @@ export const interval = (f, clear, time = 1000) => {
 
 
 
-//
-// setTimeout in promise/async skin.
-// Example usage:
-//
-// timeout(
-//     () => { console.log("Hey!"); return 42 }, 1000,
-//     (c) => timeout(() => c("Cancelled!"), 800)
-// )
-// .then((x) => console.log("Success:", x))
-// .catch((c) => console.log("Error or cancel:", c))
-//
+/**
+ * `setTimeout` in `Promise` / `async` skin.
+ *
+ * Example usage:
+ *
+ * ```
+ * timeout(
+ *     () => { console.log("Hey!"); return 42 }, 1000,
+ *     (c) => timeout(() => c("Cancelled!"), 800)
+ * )
+ * .then((x) => console.log("Success:", x))
+ * .catch((c) => console.log("Error or cancel:", c))
+ * ```
+ *
+ * @async
+ * @function timeout
+ * @param {Function} f
+ * @param {Number} [time=1000]
+ * @param {Function} [cancel]
+ * @returns {Promise}
+ */
 export const timeout = (f, time = 1000, cancel = (_canceller) => null) => {
     let
         reject = null, handle = null,
