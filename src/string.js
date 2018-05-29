c8138cc1c30eaf78e1d95ec62c954fae1a0dfba2
drmats <drmats@users.noreply.github.com>
2018-05-29 19:35:02 +0200

    string: jsdoc-style comments added.

--- src/string.js
+++ src/string.js
@@ -1,3 +1,13 @@
+/**
+ * String tools.
+ *
+ * @module string
+ * @license Apache-2.0
+ */
+
+
+
+
 import {
     draw,
     head,
@@ -10,48 +20,71 @@ import { Y } from "./func"
 
 
 
-//
-// Return full set of ASCII letters.
-//
+/**
+ * Return full set of ASCII letters.
+ *
+ * @function asciiLetters
+ * @returns {String}
+ */
 export const asciiLetters = () =>
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 
 
-//
-// Return lowercase ASCII letters.
-//
+/**
+ * Return lowercase ASCII letters.
+ *
+ * @function asciiLowercase
+ * @returns {String}
+ */
 export const asciiLowercase = () => "abcdefghijklmnopqrstuvwxyz"
 
 
 
 
-//
-// Return uppercase ASCII letters.
-//
+/**
+ * Return uppercase ASCII letters.
+ *
+ * @function asciiUppercase
+ * @returns {String}
+ */
 export const asciiUppercase = () => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 
 
-//
-// Allocate a big string (of size 2^n). Use with caution!
-//     big(16) makes 2^16 = 65536 string size.
-//     big(23) makes 2^23 = 8M string size,
-//     big(24) makes 16M and so on.
-// [c = "x"] Character used during string generation.
-//     big(2) = "xxxx"
-//     big(3, "a") = "aaaaaaaa"
-//
+/**
+ * Allocate a big string (of size 2^n). Use with caution!
+ *
+ * - `big(16)` makes `2^16 = 65536` string size.
+ * - `big(23)` makes `2^23 = 8M` string size,
+ * - `big(24)` makes `16M` and so on.
+ *
+ * `c = "x"` - Character used during string generation.
+ *
+ * ```
+ * big(2) = "xxxx"
+ * big(3, "a") = "aaaaaaaa"
+ * ```
+ *
+ * @function big
+ * @param {Number} n
+ * @param {String} c
+ * @returns {String}
+ */
 export const big = Y((r) => (n, c = "x") => n > 0  ?  r(n - 1, c + c)  :  c)
 
 
 
 
-//
-// Convert "thisKindOfText" to "ThisKindOfText".
-//
+/**
+ * Convert `thisKindOfText` to `ThisKindOfText`.
+ *
+ * @function camelToPascal
+ * @param {String} str
+ * @returns {String}
+ */
 export const camelToPascal = (str) =>
     str  &&  str.length > 0  ?
         head(str).toUpperCase() + tail(str)  :
@@ -60,18 +93,26 @@ export const camelToPascal = (str) =>
 
 
 
-//
-// Convert "thisKindOfText" to "this_kind_of_text".
-//
+/**
+ * Convert `thisKindOfText` to `this_kind_of_text`.
+ *
+ * @function camelToSnake
+ * @param {String} str
+ * @returns {String}
+ */
 export const camelToSnake = (str) =>
     str  ?  str.replace(/([A-Z])/g, "_$1").toLowerCase()  :  empty()
 
 
 
 
-//
-// Ensure given string is in form "Aaaaaaaa".
-//
+/**
+ * Ensure given string is in form `Aaaaaaaa`.
+ *
+ * @function capitalize
+ * @param {String} str
+ * @returns {String}
+ */
 export const capitalize = (str) =>
     str  &&  str.length > 0  ?
         head(str).toUpperCase() + tail(str).toLowerCase()  :
@@ -80,25 +121,35 @@ export const capitalize = (str) =>
 
 
 
-//
-// Return all digits.
-//
+/**
+ * Return all digits.
+ *
+ * @function digits
+ * @returns {String}
+ */
 export const digits = () => "0123456789"
 
 
 
 
-//
-// Construct empty string.
-//
+/**
+ * Construct empty string.
+ *
+ * @function empty
+ * @returns {String}
+ */
 export const empty = () => ""
 
 
 
 
-//
-// Convert "ThisKindOfText" to "thisKindOfText".
-//
+/**
+ * Convert `ThisKindOfText` to `thisKindOfText`.
+ *
+ * @function pascalToCamel
+ * @param {String} str
+ * @returns {String}
+ */
 export const pascalToCamel = (str) =>
     str  &&  str.length > 0  ?
         head(str).toLowerCase() + tail(str)  :
@@ -107,35 +158,53 @@ export const pascalToCamel = (str) =>
 
 
 
-//
-// Convert "ThisKindOfText" to "this_kind_of_text".
-//
+/**
+ * Convert `ThisKindOfText` to `this_kind_of_text`.
+ *
+ * @function pascalToSnake
+ * @param {String} str
+ * @returns {String}
+ */
 export const pascalToSnake = (str) =>
     str  ?  tail(camelToSnake(str))  :  empty()
 
 
 
 
-//
-// Quote text.
-//
+/**
+ * Quote text.
+ *
+ * @function quote
+ * @param {String} str
+ * @param {String} [q="\"\""]
+ * @returns {String}
+ */
 export const quote = (str, q="\"\"") => `${head(q)}${str}${last(q)}`
 
 
 
 
-//
-// Construct random string of desired length.
-//
+/**
+ * Construct random string of desired length.
+ *
+ * @function random
+ * @param {Number} [size=0]
+ * @param {String} [letters=asciiLetters()+digits()]
+ * @returns {String}
+ */
 export const random = (size = 0, letters = asciiLetters() + digits()) =>
     range(size).map(() => draw(letters.split(empty()))).join(empty())
 
 
 
 
-//
-// Convert "this_kind_of_text" to "thisKindOfText".
-//
+/**
+ * Convert `this_kind_of_text` to `thisKindOfText`.
+ *
+ * @function snakeToCamel
+ * @param {String} str
+ * @returns {String}
+ */
 export const snakeToCamel = (str) => (
     (pascal) =>
         pascal.length > 0  ?
@@ -146,9 +215,13 @@ export const snakeToCamel = (str) => (
 
 
 
-//
-// Convert "this_kind_of_text" to "ThisKindOfText".
-//
+/**
+ * Convert `this_kind_of_text` to `ThisKindOfText`.
+ *
+ * @function snakeToPascal
+ * @param {String} str
+ * @returns {String}
+ */
 export const snakeToPascal = (str) =>
     str  ?
         str.split(/_+/g).map((w) => capitalize(w)).join(empty())  :
