89e248bb6b0710e67ba7e6ced6a107ada85505e7
drmats <drmats@users.noreply.github.com>
2018-10-09 16:32:18 +0200

    Config: babelrc refactored.

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,24 @@
+// common plugins
+var commonPlugins = [
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-arrow-functions",
+    "@babel/plugin-transform-block-scoped-functions",
+    "@babel/plugin-transform-block-scoping",
+    "@babel/plugin-transform-computed-properties",
+    "@babel/plugin-transform-destructuring",
+    "@babel/plugin-transform-exponentiation-operator",
+    "@babel/plugin-transform-literals",
+    "@babel/plugin-transform-parameters",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-shorthand-properties",
+    "@babel/plugin-transform-spread",
+    "@babel/plugin-transform-template-literals"
+];
+
+
+
+
+// configuration
 module.exports = function (api) {
     api.cache.using(() => process.env.BABEL_ENV);
     console.log("Compiling for", "'" + api.env() + "'", "...");
@@ -6,22 +27,9 @@ module.exports = function (api) {
         "env": {
             "commonjs": {
                 "comments": false,
-                "plugins": [
-                    "@babel/plugin-proposal-object-rest-spread",
-                    "@babel/plugin-transform-arrow-functions",
-                    "@babel/plugin-transform-block-scoped-functions",
-                    "@babel/plugin-transform-block-scoping",
-                    "@babel/plugin-transform-computed-properties",
-                    "@babel/plugin-transform-destructuring",
-                    "@babel/plugin-transform-exponentiation-operator",
-                    "@babel/plugin-transform-literals",
-                    "@babel/plugin-transform-modules-commonjs",
-                    "@babel/plugin-transform-parameters",
-                    "@babel/plugin-transform-runtime",
-                    "@babel/plugin-transform-shorthand-properties",
-                    "@babel/plugin-transform-spread",
-                    "@babel/plugin-transform-template-literals"
-                ],
+                "plugins": commonPlugins.concat([
+                    "@babel/plugin-transform-modules-commonjs"
+                ]),
                 "presets": [
                     [
                         "@babel/preset-env",
@@ -37,21 +45,7 @@ module.exports = function (api) {
             },
             "es": {
                 "comments": false,
-                "plugins": [
-                    "@babel/plugin-proposal-object-rest-spread",
-                    "@babel/plugin-transform-arrow-functions",
-                    "@babel/plugin-transform-block-scoped-functions",
-                    "@babel/plugin-transform-block-scoping",
-                    "@babel/plugin-transform-computed-properties",
-                    "@babel/plugin-transform-destructuring",
-                    "@babel/plugin-transform-exponentiation-operator",
-                    "@babel/plugin-transform-literals",
-                    "@babel/plugin-transform-parameters",
-                    "@babel/plugin-transform-runtime",
-                    "@babel/plugin-transform-shorthand-properties",
-                    "@babel/plugin-transform-spread",
-                    "@babel/plugin-transform-template-literals"
-                ],
+                "plugins": commonPlugins,
                 "presets": [
                     [
                         "@babel/preset-env",
