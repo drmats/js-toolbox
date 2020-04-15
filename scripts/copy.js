c80386a2990506b454e2767aef71ad98d2e71c43
drmats <drmats@users.noreply.github.com>
2020-04-15 15:09:50 +0200

    config: copy/delete scripts adjusted to handle type declaration files.

--- scripts/copy.js
+++ scripts/copy.js
@@ -3,10 +3,13 @@
 
 
 
-var copy = require("fs").copyFileSync
+var copy = require("fs").copyFileSync;
 
 
-// copy src modules to package root
-require("./module_names").forEach(
-    mn => copy("./src/" + mn + ".js", "./" + mn + ".js")
-)
+
+
+// copy src type files to package root
+["index"].concat(require("./module_names"))
+    .forEach(
+        mn => copy("./src/" + mn + ".d.ts", "./" + mn + ".d.ts")
+    );
--- scripts/delete.js
+++ scripts/delete.js
@@ -5,12 +5,14 @@
 
 var
     rm = require("fs").unlinkSync,
-    ignoreError = f => (...args) => { try { f(...args) } catch (_) { _ } }
+    ignoreError = f => (...args) => { try { f(...args) } catch (_) { _ } },
+    append = (xs, ys) => { Array.prototype.push.apply(xs, ys); return xs };
 
 
 
 
-// remove src modules from package root
-require("./module_names").forEach(
-    mn => ignoreError(rm) ("./" + mn + ".js")
-)
+// remove src modules and type files from package root
+["index"].concat(require("./module_names"))
+    .map(mn => ["./" + mn + ".js", "./" + mn + ".d.ts"])
+    .reduce(append, [])
+    .forEach(ignoreError(rm));
