32a9accfef0882cc0474d7ef63b43ffb3480f33a
drmats <drmats@users.noreply.github.com>
2020-04-16 12:42:52 +0200

    config: dist package.json created dynamically during build.

--- scripts/stuff.js
+++ scripts/stuff.js
@@ -18,7 +18,11 @@ var
         "sideEffects": false,
         "module": `../es/${mn}/index.js`,
         "typings": "./index.d.ts",
-    });
+    }),
+
+    packageJson = require("../package.json"),
+
+    distJson = require("../dist.template.json");
 
 
 
@@ -40,11 +44,26 @@ require("./module_names")
         );
     });
 
+
 // copy index type file
 copyFileSync(`${srcDir}/index.d.ts`, `${distDir}/index.d.ts`);
 
-// copy main package config file
-copyFileSync("./dist.template.json", `${distDir}/package.json`);
+
+// prepare dist package.json and store it
+[
+    "bugs", "contributors", "dependencies", "description",
+    "engines", "homepage", "keywords", "license", "name",
+    "repository", "version"
+].forEach(key => { distJson[key] = packageJson[key] });
+writeFileSync(
+    `${distDir}/package.json`,
+    JSON.stringify(
+        Object.entries(distJson)
+            .sort(([a, _1], [b, _2]) => a.localeCompare(b))
+            .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+    )
+);
+
 
 // copy README.md and LICENSE
 copyFileSync("./README.md", `${distDir}/README.md`);
