50d006d4dd37004c5dac8755ad2abef8d7e2aa63
drmats <drmats@users.noreply.github.com>
2020-04-16 12:20:33 +0200

    config: production package configuration template.

--- scripts/stuff.js
+++ scripts/stuff.js
@@ -3,13 +3,18 @@
 
 
 
+// ...
 var
+
     {
         copyFileSync,
         writeFileSync,
     } = require("fs"),
 
-    packageTemplate = mn => ({
+    srcDir = "./src",
+    distDir = "./dist",
+
+    esConfig = mn => ({
         "sideEffects": false,
         "module": `../es/${mn}/index.js`,
         "typings": "./index.d.ts",
@@ -18,21 +23,34 @@ var
 
 
 
-console.info("Copying type declarations and es configs ...")
+console.info("Copying type declarations and module configs ...");
+
+
+
 
 // copy src type files and es configs to dist directory
 require("./module_names")
     .forEach(mn => {
-        let src = `./src/${mn}`, dst = `./dist/${mn}`
+        let src = `${srcDir}/${mn}`, dst = `${distDir}/${mn}`
         copyFileSync(`${src}/${mn}.d.ts`, `${dst}/${mn}.d.ts`);
         copyFileSync(`${src}/index.d.ts`, `${dst}/index.d.ts`);
         writeFileSync(
             `${dst}/package.json`,
-            JSON.stringify(packageTemplate(mn))
+            JSON.stringify(esConfig(mn))
         );
     });
 
 // copy index type file
-copyFileSync("./src/index.d.ts", "./dist/index.d.ts")
+copyFileSync(`${srcDir}/index.d.ts`, `${distDir}/index.d.ts`);
 
-console.log("OK.")
+// copy main package config file
+copyFileSync("./dist.template.json", `${distDir}/package.json`);
+
+// copy README.md and LICENSE
+copyFileSync("./README.md", `${distDir}/README.md`);
+copyFileSync("./LICENSE", `${distDir}/LICENSE`);
+
+
+
+
+console.log("OK.");
