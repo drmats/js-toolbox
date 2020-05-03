720cabf0351200fa36633d6934e6b0eb6936df9a
drmats <drmats@users.noreply.github.com>
2020-05-03 18:01:31 +0200

    Config: babel configuration simplified.

--- babel.config.js
+++ babel.config.js
@@ -6,20 +6,20 @@
 // common plugins
 var commonPlugins = [
     "@babel/plugin-proposal-export-namespace-from",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-transform-arrow-functions",
-    "@babel/plugin-transform-block-scoped-functions",
-    "@babel/plugin-transform-block-scoping",
-    "@babel/plugin-transform-computed-properties",
-    "@babel/plugin-transform-destructuring",
-    "@babel/plugin-transform-exponentiation-operator",
-    "@babel/plugin-transform-literals",
-    "@babel/plugin-transform-parameters",
+    // "@babel/plugin-proposal-object-rest-spread",
+    // "@babel/plugin-syntax-dynamic-import",
+    // "@babel/plugin-transform-arrow-functions",
+    // "@babel/plugin-transform-block-scoped-functions",
+    // "@babel/plugin-transform-block-scoping",
+    // "@babel/plugin-transform-computed-properties",
+    // "@babel/plugin-transform-destructuring",
+    // "@babel/plugin-transform-exponentiation-operator",
+    // "@babel/plugin-transform-literals",
+    // "@babel/plugin-transform-parameters",
     "@babel/plugin-transform-runtime",
-    "@babel/plugin-transform-shorthand-properties",
-    "@babel/plugin-transform-spread",
-    "@babel/plugin-transform-template-literals",
+    // "@babel/plugin-transform-shorthand-properties",
+    // "@babel/plugin-transform-spread",
+    // "@babel/plugin-transform-template-literals",
     "babel-plugin-inline-json-import",
 ]
 
@@ -30,16 +30,14 @@ var commonPlugins = [
 module.exports = function (api) {
     api.cache.using(() => process.env.BABEL_ENV)
     // eslint-disable-next-line
-    console.log("Compiling for", "'" + api.env() + "'", "...")
+    console.log(`Compiling for '${api.env()}' ...`)
 
     return {
         env: {
             commonjs: {
                 comments: false,
                 shouldPrintComment: () => false,
-                plugins: commonPlugins.concat([
-                    "@babel/plugin-transform-modules-commonjs",
-                ]),
+                plugins: commonPlugins,
                 presets: [
                     [
                         "@babel/preset-env",
@@ -47,7 +45,7 @@ module.exports = function (api) {
                             modules: "commonjs",
                             shippedProposals: true,
                             targets: {
-                                node: "8.0.0",
+                                node: "10.0.0",
                             },
                         },
                     ],
