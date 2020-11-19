d7e5bb2ca6013593c519d63b30bc4707ed840220
drmats <drmats@users.noreply.github.com>
2020-11-19 14:30:02 +0100

    utils: 'jss' submodule converted to .ts.

---
+++ src/utils/jss.ts
@@ -0,0 +1,51 @@
+/**
+ * Various tools.
+ *
+ * @module utils
+ * @license Apache-2.0
+ * @author drmats
+ */
+
+
+
+
+import {
+    quote,
+    wrap,
+} from "../string/transform";
+
+
+
+
+/**
+ * JSS color helper.
+ *
+ * @function rgb
+ * @returns Wrapped JSS rgb() string.
+ */
+export const rgb = (r: number, g: number, b: number): string =>
+    wrap([r, g, b].join(", "), "rgb(", ")");
+
+
+
+
+/**
+ * JSS color helper (with alpha).
+ *
+ * @function rgba
+ * @returns Wrapped JSS rgba() string.
+ */
+export const rgba = (r: number, g: number, b: number, a: number): string =>
+    wrap([r, g, b, a].join(", "), "rgba(", ")");
+
+
+
+
+/**
+ * JSS url helper.
+ *
+ * @function url
+ * @returns Wrapped JSS url() string.
+ */
+export const url = (x: string): string =>
+    wrap(quote(x), "url(", ")");
