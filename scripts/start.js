d8e33c442db0dc60a53caa2163835a100daee143
drmats <drmats@users.noreply.github.com>
2020-11-20 22:23:45 +0100

    scripts: start - submodules sorted in each module.

--- scripts/start.js
+++ scripts/start.js
@@ -6,7 +6,10 @@
 // inject compiled modules to repl context
 Object.assign(
     require("repl").start({}).context,
-    require("./module_names").reduce(
-        (e, mn) => (e[mn] = require("../dist/" + mn), e), {}
-    )
+    require("./module_names").reduce((e, mn) => (
+        e[mn] = Object.entries(require("../dist/" + mn).default)
+            .sort(([k1], [k2]) => k1.localeCompare(k2))
+            .reduce((m, [k, v]) => (m[k] = v, m), {}),
+        e
+    ), {})
 );
