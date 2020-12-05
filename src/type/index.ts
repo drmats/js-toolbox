706554567a7ffe9882660fb3fe7f652a856f20ac
drmats <drmats@users.noreply.github.com>
2020-12-05 13:05:40 +0100

    type: list - exported.

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,7 +1,9 @@
 import * as check from "./check";
 import * as consts from "./consts";
 import * as conv from "./conv";
+import * as list from "./list";
 export * from "./check";
 export * from "./consts";
 export * from "./conv";
-export default Object.assign({}, check, consts, conv);
+export * from "./list";
+export default Object.assign({}, check, consts, conv, list);
