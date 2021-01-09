9921ef672ac140c87b04b93c080b865a5d31ac9b
drmats <drmats@users.noreply.github.com>
2021-01-09 10:49:42 +0100

    array: random - transformed to typescript.

--- src/array/random.ts
+++ src/array/random.ts
@@ -9,14 +9,16 @@
 
 
 
-import { range } from "./tools"
+import type { ArrStr } from "../type/defs";
+import type { ChooseArrElOrStr } from "../type/utils";
+import { range } from "./tools";
 import {
     dec,
     inc,
     sub,
-} from "../math/arithmetic"
-import { randomInt } from "../math/random"
-import { isArray } from "../type/check"
+} from "../math/arithmetic";
+import { randomInt } from "../math/random";
+import { isArray } from "../type/check";
 
 
 
@@ -28,9 +30,9 @@ import { isArray } from "../type/check"
  * @param {Array<T>|String} arr
  * @returns {T|String}
  */
-export const draw = xs => {
-    if (!xs.length) throw new TypeError("array.draw() - empty list")
-    return xs[randomInt() % xs.length]
+export function draw<T extends ArrStr> (xs: T): ChooseArrElOrStr<T> {
+    if (!xs.length) throw new TypeError("array.draw() - empty list");
+    return xs[randomInt() % xs.length];
 }
 
 
@@ -46,17 +48,17 @@ export const draw = xs => {
  * @param {Array} arr
  * @returns {Array}
  */
-export const shuffle = arr => {
+export function shuffle<T> (arr: T[]): T[] {
     if (!isArray(arr)) throw new TypeError(
         `array.shuffle() - expected array as argument, got [${typeof arr}]`
-    )
+    );
 
     for (let i = dec(arr.length);  i > 0;  i -= 1) {
         let j = randomInt() % inc(i);
-        [arr[i], arr[j]] = [arr[j], arr[i]]
+        [arr[i], arr[j]] = [arr[j], arr[i]];
     }
 
-    return arr
+    return arr;
 }
 
 
@@ -77,31 +79,33 @@ export const shuffle = arr => {
  * @param {Number} size
  * @returns {Array}
  */
-export const sparse = (...args) => {
+export function sparse (stop: number, size: number): number[];
+export function sparse (start: number, stop: number, size: number): number[];
+export function sparse (...args: number[]): number[] {
     let
         start = 0, stop = 0, size = 0,
-        hash = Object.create(null),
-        interval = 0
+        hash = Object.create(null) as Record<number, number>,
+        interval = 0;
 
-    if (args.length === 2) { [stop, size] = args }
-    else if (args.length === 3) { [start, stop, size] = args }
+    if (args.length === 2) { [stop, size] = args; }
+    else if (args.length === 3) { [start, stop, size] = args; }
     else throw new TypeError(
         `array.sparse() expected 2 or 3 arguments, got ${args.length}`
-    )
+    );
 
-    if (start > stop) { [start, stop] = [stop, start] }
-    interval = stop - start
+    if (start > stop) { [start, stop] = [stop, start]; }
+    interval = stop - start;
 
-    if (size <= 0  ||  interval === 0) { return [] }
-    if (size >= interval) { return range(start, stop) }
+    if (size <= 0  ||  interval === 0) { return []; }
+    if (size >= interval) { return range(start, stop); }
 
     while (size > 0) {
-        let val = (randomInt() % interval) + start
+        let val = (randomInt() % interval) + start;
         if (!Object.hasOwnProperty.call(hash, val)) {
-            hash[val] = val
-            size = dec(size)
+            hash[val] = val;
+            size = dec(size);
         }
     }
 
-    return Object.values(hash).sort(sub)
+    return Object.values(hash).sort(sub);
 }
